ca66fcfeb4f7d650bebf92df3f87be00ecb3b697
bdjones2410 <brandon.jones@blackbaud.com>
2018-11-06 11:37:10 -0500

    updated tests and dependencies

--- src/code-block.js
+++ src/code-block.js
@@ -1,32 +1,36 @@
 const cheerio = require('cheerio');
 
+const convertToHTMLEntities = (content) => {
+  return content.replace(/{/g, `{{ '{' }}`)
+    .replace(/</g, '&lt;');
+}
+
+const cheerioConfig = {
+  lowerCaseTags: false,
+  lowerCaseAttributeNames: false,
+  decodeEntities: false
+};
+
 const preload = (content, resourcePath) => {
   if (!resourcePath.match(/\.html$/)) {
     return content;
   }
 
-  let $ = cheerio.load(content, {
-    lowerCaseTags: false,
-    lowerCaseAttributeNames: false,
-    decodeEntities: false
-  });
+  const $ = cheerio.load(content, cheerioConfig);
+  const codeBlocks = $('stache-code-block');
 
-  let codeBlocks = $('stache-code-block');
-
-  if (codeBlocks.length) {
-    codeBlocks.each(function () {
-      let innerText = $(this).html()
-        .toString()
-        .replace(/{/g, `{{ '{' }}`)
-        .replace(/</g, '&lt;');
-
-      $(this).text(innerText);
-    });
-
-    content = $.html().toString();
+  if (!codeBlocks.length) {
+    return content;
   }
 
-  return content;
+  codeBlocks.each((idx, elem) => {
+    const $elem = $(elem);
+    const rawContent = $elem.html().toString();
+    const content = convertToHTMLEntities(rawContent);
+    $elem.html(content);
+  });
+
+  return $.html().toString();
 };
 
 module.exports = { preload };
